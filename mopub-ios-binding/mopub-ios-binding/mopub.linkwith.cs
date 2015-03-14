ef9287a0357186569f049cb0aa970dc6edc6578e
Ian Levesque <ian@ianlevesque.org>
2015-03-14 04:32:22 -0400

    Fix linking, add Google & iAds, add Frameworks.

--- mopub-ios-binding/mopub-ios-binding/mopub.linkwith.cs
+++ mopub-ios-binding/mopub-ios-binding/mopub.linkwith.cs
@@ -1,4 +1,9 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith ("mopub.a", LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.ArmV7 | LinkTarget.Arm64, SmartLink = true, ForceLoad = true)]
+[assembly: LinkWith (
+    "mopub.a", 
+    LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.ArmV7 | LinkTarget.Arm64, 
+    SmartLink = true, 
+    ForceLoad = true,
+    Frameworks = "AVFoundation AudioToolbox CoreGraphics CoreLocation CoreTelephony EventKit EventKitUI Foundation MediaPlayer MessageUI QuartzCore StoreKit SystemConfiguration UIKit iAd AdSupport StoreKit")]
