ca4ba6381c1d37efabc0edab792fb720804c35a2
Ian Levesque <ian@ianlevesque.org>
2015-03-17 06:34:50 -0400

    Support older OS versions, update included mopub binary.

--- mopub-ios-binding/mopub-ios-binding/mopub.linkwith.cs
+++ mopub-ios-binding/mopub-ios-binding/mopub.linkwith.cs
@@ -4,6 +4,6 @@ using ObjCRuntime;
 [assembly: LinkWith (
     "mopub.a", 
     LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.ArmV7 | LinkTarget.Arm64, 
-    SmartLink = true, 
     ForceLoad = true,
-    Frameworks = "AVFoundation AudioToolbox CoreGraphics CoreLocation CoreTelephony EventKit EventKitUI Foundation MediaPlayer MessageUI QuartzCore StoreKit SystemConfiguration UIKit iAd AdSupport StoreKit")]
+    Frameworks = "AudioToolbox AVFoundation CoreGraphics CoreLocation CoreTelephony EventKit EventKitUI Foundation iAd MediaPlayer MessageUI MobileCoreServices QuartzCore SystemConfiguration UIKit",
+    WeakFrameworks = "AdSupport StoreKit PassKit Social")]
