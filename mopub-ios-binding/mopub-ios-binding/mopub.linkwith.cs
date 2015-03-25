ac69621a9a1cea86d7ce17407388787d723a168c
Ian Levesque <ian@ianlevesque.org>
2015-03-25 13:33:08 -0400

    Enable SmartLink.

--- mopub-ios-binding/mopub-ios-binding/mopub.linkwith.cs
+++ mopub-ios-binding/mopub-ios-binding/mopub.linkwith.cs
@@ -4,6 +4,6 @@ using ObjCRuntime;
 [assembly: LinkWith (
     "mopub.a", 
     LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.ArmV7 | LinkTarget.Arm64, 
-    ForceLoad = true,
+    SmartLink = true,
     Frameworks = "AudioToolbox AVFoundation CoreGraphics CoreLocation CoreTelephony EventKit EventKitUI Foundation iAd MediaPlayer MessageUI MobileCoreServices QuartzCore SystemConfiguration UIKit",
     WeakFrameworks = "AdSupport StoreKit PassKit Social")]
