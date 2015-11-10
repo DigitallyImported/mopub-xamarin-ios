f520be4548ccd4dc768527ab35cbb34bec653113
Ian Levesque <ian@ianlevesque.org>
2015-11-10 02:03:48 -0500

    Had better luck with an iOS 8 dynamic framework.

--- mopub-ios-binding/mopub-ios-binding/mopub.linkwith.cs
+++ mopub-ios-binding/mopub-ios-binding/mopub.linkwith.cs
@@ -4,6 +4,6 @@ using ObjCRuntime;
 [assembly: LinkWith (
     "mopub.a", 
     LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.ArmV7 | LinkTarget.Arm64, 
-    SmartLink = true,
+	ForceLoad = true,
     Frameworks = "AudioToolbox AVFoundation CoreGraphics CoreLocation CoreTelephony EventKit EventKitUI Foundation iAd MediaPlayer MessageUI MobileCoreServices QuartzCore SystemConfiguration UIKit",
 	WeakFrameworks = "AdSupport CoreBluetooth PassKit Social StoreKit")]
